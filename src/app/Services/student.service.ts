2d0c3899fe6f197522c3b8023722aaf8b3faef45
davidamankwah <G00394825@gmit.ie>
2022-03-13 12:44:31 +0000

    lab upload

---
+++ src/app/Services/student.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import{Observable} from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class StudentService {
+
+  constructor(private http:HttpClient) { }
+
+  GetStudentData():Observable<any>{
+    return this.http.get('https://jsonblob.com/api/jsonblob/952205759878545408');
+  }
+
+  GetWeatherData():Observable<any>{
+    return this.http.get('http://api.openweathermap.org/data/2.5/weather?q=Galway&APPID=0c4f5b15c7c54d1d7fdb562e1e7fbdad');
+  }
+}
